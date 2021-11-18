f99fd7607a273c112f2b5dd8bafc409e5652a0e0
LABO13-10-USER <LABO13-10-USER@satis.local>
2021-11-18 21:17:01 +0100

    v4

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice php</title>
+    <link href="info.php">
+</head>
+<body>
+    <?php  
+    echo "<a href=\"info.php\">Info php!</a>";
+    ?>
+</body>
+</html>
